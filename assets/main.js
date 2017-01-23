85f1978509d7efa8c9a5959f6b297a762a45603e
mazi-mazur <mm.michal.mazur@gmail.com>
2017-01-23 21:13:52 +0000

    First message

--- assets/main.js
+++ assets/main.js
@@ -1,5 +1,9 @@
 $(function() {
-
+  $.ajax({
+    url: 'https://www.codeschool.com/users/mazi_mazur.json',
+    dataType: 'jsonp',
+    success:function(response){}
+  });
   // your code will go here
 
 });
