20383f1d82e8707efe3182367b406fc28e7f9bb3
mazi-mazur <mm.michal.mazur@gmail.com>
2017-01-25 22:17:50 +0000

    Added h3

--- src/assets/main.js
+++ src/assets/main.js
@@ -3,16 +3,23 @@ $(function() {
     url: 'https://www.codeschool.com/users/mazi_mazur.json',
     dataType: 'jsonp',
     success:function(response){
-      console.log('response', response.courses.completed);
       addCourses(response.courses.completed);
     }
   });
+
   function addCourses(courses) {
+
     var $badges = $('#badges');
-    courses.forEach(function(course){
-      $('<div></div>', {
+
+    courses.forEach(function(course) {
+
+      var $course = $('<div />', {
          'class': 'course'
        }).appendTo($badges);
+
+     $('<h3 />', {
+       text: course.title
+     }).appendTo($course);
     })
 
   }
